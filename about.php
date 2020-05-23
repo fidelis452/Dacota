36004647bf9c0ce8dfff609b13215852efd8815f
Fidel <fideliswaweru19@gmail.com>
2020-05-23 22:48:50 +0300

    commit2

---
+++ about.php
@@ -0,0 +1,308 @@
+<?php include('include/header.php');?>
+<!-- main header end -->
+
+<!-- Sub banner start -->
+<div class="sub-banner overview-bgi">
+    <div class="container">
+        <div class="breadcrumb-area">
+            <h1>About Us</h1>
+            <ul class="breadcrumbs">
+                <li><a href="index.php">Home</a></li>
+                <li class="active">About Us</li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- Sub banner end -->
+
+<!-- About us start -->
+<div class="about-us content-area-8 bg-white">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5">
+                <div class="properties-service-v">
+                    <img src="assets/img/ava.jpg" alt="admin" class="img-fluid">
+                </div>
+            </div>
+            <div class="col-lg-7 align-self-center">
+                <div class="about-text more-info">
+                    <h3>Why Choose Us?</h3>
+                    <div id="faq" class="faq-accordion">
+                        <div class="card m-b-0">
+                        <div class="card-header">
+                                <a class="card-title collapsed" data-toggle="collapse" data-parent="#faq" href="#collapse2">
+                                    Team work
+                                </a>
+                            </div>
+                            <div id="collapse2" class="card-block collapse">
+                                <p></p>
+                            </div>
+
+                            <div class="card-header">
+                                <a class="card-title collapsed" data-toggle="collapse" data-parent="#faq" href="#collapse2">
+                                    Charity programs
+                                </a>
+                            </div>
+                            <div id="collapse2" class="card-block collapse">
+                                <p>Nothing yet</p>
+                            </div>
+
+                            <div class="card-header">
+                                <a class="card-title collapsed" data-toggle="collapse" data-parent="#faq" href="#collapse3">
+                                    Honesty and integrity
+                                </a>
+                            </div>
+                            <div id="collapse3" class="card-block collapse">
+                                <p></p>
+                            </div>
+
+                            <div class="card-header bd-none">
+                                <a class="card-title collapsed" data-toggle="collapse" data-parent="#faq" href="#collapse4">
+                                    Online donation system with different methods
+                                </a>
+                            </div>
+                            <div id="collapse4" class="card-block collapse">
+                                <p></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- About us end -->
+
+<!-- agent start -->
+<div class="agent content-area-2">
+    <div class="container">
+        <div class="main-title">
+            <h1>Meet Our Agents</h1>
+            <p>The Team</p>
+        </div>
+        <div class="row">
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+                <div class="agent-2">
+                    <div class="agent-photo">
+                        <a href="#">
+                            <img src="assets/img/ima.png" alt="avatar-5" height="300px" width="180px">
+                        </a>
+                    </div>
+                    <div class="agent-details">
+                        <h5><a href="#">Fidelis Waweru</a></h5>
+                        <p>Web Developer</p>
+                        <ul class="social-list clearfix">
+                            <li><a href="#" class="facebook"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#" class="twitter"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#" class="instagram"><i class="fa fa-instagram"></i></a></li>
+                            <li><a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+                <div class="agent-2">
+                    <div class="agent-photo">
+                        <a href="#">
+                            <img src="assets/img/jenny.jpg" alt="avatar-6" height="300px" width="180px">
+                        </a>
+                    </div>
+                    <div class="agent-details">
+                        <h5><a href="#">Jennifer Kibiri</a></h5>
+                        <p>Web Developer</p>
+                        <ul class="social-list clearfix">
+                            <li><a href="#" class="facebook"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#" class="twitter"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#" class="instagram"><i class="fa fa-instagram"></i></a></li>
+                            <li><a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+                <div class="agent-2">
+                    <div class="agent-photo">
+                        <a href="#">
+                            <img src="assets/img/ava.jpg" alt="avatar-7" height="300px" width="180px">
+                        </a>
+                    </div>
+                    <div class="agent-details">
+                        <h5><a href="#">Christopher Munene</a></h5>
+                        <p>Office Manager</p>
+                        <ul class="social-list clearfix">
+                            <li><a href="#" class="facebook"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#" class="twitter"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#" class="instagram"><i class="fa fa-instagram"></i></a></li>
+                            <li><a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+                <div class="agent-2">
+                    <div class="agent-photo">
+                        <a href="#">
+                            <img src="assets/img/ava.jpg" alt="avatar-11" height="300px" width="180px">
+                        </a>
+                    </div>
+                    <div class="agent-details">
+                        <h5><a href="#">Karen Paran</a></h5>
+                        <p>Support Manager</p>
+                        <ul class="social-list clearfix">
+                            <li><a href="#" class="facebook"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#" class="twitter"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#" class="instagram"><i class="fa fa-instagram"></i></a></li>
+                            <li><a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div >
+<!-- agent end -->
+
+<!-- Counters start 
+<div class="counters overview-bgi" style="background-image: url(assets/img/bg-photo-2.jpg)">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <div class="counter-box">
+                    <i class="flaticon-tag"></i>
+                    <h1 class="counter">500</h1>
+                    <h5>Lines of Sale</h5>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <div class="counter-box">
+                    <i class="flaticon-badge"></i>
+                    <h1 class="counter">254</h1>
+                    <h5>Listings For Rent</h5>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <div class="counter-box">
+                    <i class="flaticon-call-center-agent"></i>
+                    <h1 class="counter">510</h1>
+                    <h5>Agents</h5>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <div class="counter-box">
+                    <i class="flaticon-job"></i>
+                    <h1 class="counter">94</h1>
+                    <h5>Brokers</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Counters end -->
+
+<!-- Testimonial 3 start -->
+<div class="testimonial testimonial-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="testimonial-inner">
+                    <header class="testimonia-header">
+                        <h1>Testimonial</h1>
+                    </header>
+                    <div id="carouselExampleIndicators2" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-target="#carouselExampleIndicators2" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselExampleIndicators2" data-slide-to="1"></li>
+                            <li data-target="#carouselExampleIndicators2" data-slide-to="2"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <p class="lead">
+                                “A very interesting and haunting visual experience!'It stats with you a long time.
+                                        Very, very unique. A very interesting and haunting visual experience!'It stats
+                                         with you a long time. Very, very unique.”
+                                </p>
+                                <ul class="rating">
+                                    <li>
+                                        <i class="fa fa-star"></i>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-star"></i>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-star"></i>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-star"></i>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-star-half-full"></i>
+                                    </li>
+                                </ul>
+                                <div class="author-name">
+                                    Kelly Johns
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <p class="lead">
+                                “I am in love with the home designs. They are what i've been looking for a long time! I am 
+                                            in love with the home designs. They are what i've been looking for a long time!”
+                                </p>
+                                <ul class="rating">
+                                    <li>
+                                        <i class="fa fa-star"></i>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-star"></i>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-star"></i>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-star"></i>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-star-half-full"></i>
+                                    </li>
+                                </ul>
+                                <div class="author-name">
+                                    Nelly Anyango
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <p class="lead">
+                                “A very interesting and haunting visual experience!'It stats with you a long time.
+                                        Very, very unique. A very interesting and haunting visual experience!'It stats
+                                         with you a long time. Very, very unique.”
+                                </p>
+                                <ul class="rating">
+                                    <li>
+                                        <i class="fa fa-star"></i>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-star"></i>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-star"></i>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-star"></i>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-star-half-full"></i>
+                                    </li>
+                                </ul>
+                                <div class="author-name">
+                                    Karen Munene
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Testimonial 3 end -->
+
+<!-- Footer start -->
+<?php include('include/footer.php');?>
